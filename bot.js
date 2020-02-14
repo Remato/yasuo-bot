63ffb82aeaa33e7ee2c47dcb24e5182284e9b23f
Remato <renatobcostaa@gmail.com>
2020-02-14 02:40:13 -0300

    botzinho chama no x1 :(

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Twit = require('twit')
+const config = require('./config');
+
+const schedule = require('node-schedule');
+
+var T = new Twit(config);
+const reply = 'X1 lixo?';
+
+
+var job = schedule.scheduleJob({ hour: 8, minute: 00 }, function(){
+  T.get('search/tweets', 
+ {
+  q: 'yasuo lixo',
+  count: 5,
+  result_type: 'recent',
+ },
+ function(err, data, response) {
+  data.statuses.forEach(element => {
+    T.post('statuses/update',
+    {
+     status: reply,
+     attachment_url: `https://twitter.com/${element.user.screen_name}/status/${element.id_str}`,
+    })
+  })
+ });
+});
+
+console.log(job);
+
+
